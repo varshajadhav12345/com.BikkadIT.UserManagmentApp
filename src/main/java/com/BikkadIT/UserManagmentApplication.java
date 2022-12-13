53908261b886c7a872b6280bc9f423f470907da1
varshajadhav12345 <virshajadhav565@gmail.com>
2022-12-13 21:28:00 +0530

    UMA-3:create Spring-bootb App with belo Dependancy

---
+++ src/main/java/com/BikkadIT/UserManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagmentApplication.class, args);
+	}
+
+}
